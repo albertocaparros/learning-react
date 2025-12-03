d9729c7327031e74301600eb389b3bfd29394e6b
Alberto Caparrós Trigo <Alberto.Caparros.Trigo@unit4.com>
2025-12-03 21:25:22 +0100

    Move the state down to list component

--- src/App.jsx
+++ src/App.jsx
@@ -3,39 +3,50 @@ import { initialTodos, createTodo } from './todos.js';
 
 export default function TodoList() {
   const [todos, setTodos] = useState(initialTodos);
+  console.log('rendered');
+
+  return (
+    <>    
+      <NewTodo onAdd={newTodo => setTodos([...todos, newTodo])} />
+      <List todos={todos} />
+    </>
+  );
+}
+
+function List({ todos }) {
+  console.log("render List");
+
   const [showActive, setShowActive] = useState(false);
 
   const activeTodos = () => todos.filter(todo => !todo.completed);
   const visibleTodos = showActive ? activeTodos() : todos;
 
-  console.log('rendered');
-
   return (
-    <>
-      <label>
-        <input
-          type="checkbox"
-          checked={showActive}
-          onChange={e => setShowActive(e.target.checked)}
-        />
-        Show only active todos
-      </label>
-      <NewTodo onAdd={newTodo => setTodos([...todos, newTodo])} />
-      <ul>
-        {visibleTodos.map(todo => (
-          <li key={todo.id}>
-            {todo.completed ? <s>{todo.text}</s> : todo.text}
-          </li>
-        ))}
-      </ul>
-      <footer>
-        {activeTodos().length} todos left
-      </footer>
-    </>
-  );
+      <>
+        <label>
+          <input
+            type="checkbox"
+            checked={showActive}
+            onChange={e => setShowActive(e.target.checked)}
+          />
+          Show only active todos
+        </label>
+        <ul>
+          {visibleTodos.map(todo => (
+            <li key={todo.id}>
+              {todo.completed ? <s>{todo.text}</s> : todo.text}
+            </li>
+          ))}
+        </ul>
+        <footer>
+          {activeTodos().length} todos left
+        </footer>
+      </>
+    )
 }
 
 function NewTodo({ onAdd }) {
+  console.log("Render NewTodo");
   const [text, setText] = useState('');
 
   function handleAddClick() {
